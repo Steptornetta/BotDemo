109cdb462cc075dc8d058e81f8e36480df6de272
Steve Tornetta <steptornetta@yahoo.com>
2021-01-25 15:21:15 -0500

    First Commit

---
+++ src/botdemo.py
@@ -0,0 +1,15 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Tornetta"
+__date__ = "$Jan 25, 2021 1:36:37 PM$"
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+time.sleep(5)
+driver.close()
---
+++ src/setup.py
@@ -0,0 +1,27 @@
+__author__ = "Tornetta"
+__date__ = "$Jan 25, 2021 2:01:44 PM$"
+
+from setuptools import setup, find_packages
+
+setup (
+       name='BotDemo',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['foo>=3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='Tornetta',
+       author_email='',
+
+       summary='Just another Python package for the cheese shop',
+       url='',
+       license='',
+       long_description='Long description of the package',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
